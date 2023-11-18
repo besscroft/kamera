c5a8a9a7fedb040a327af5b95375df7dbe1bcaa8
besscroft <besscroft@foxmail.com>
2023-11-18 17:44:01 +0800

    feat:新的 logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,7 +74,7 @@ export default defineNuxtConfig({
       viewport: 'width=device-width,initial-scale=1',
       link: [
         { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
         { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
       ],
       meta: [
