a13ec874030049e90bae350a8246372b51d13486
besscroft <besscroft@foxmail.com>
2023-12-12 16:28:03 +0800

    fix:修复 layout 高度问题

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -9,7 +9,7 @@ const smAndLarger = breakpoints.greaterOrEqual('md')
 <template>
   <div>
     <Header />
-    <div style="height: calc(100vh - 10rem);" pb-14>
+    <div style="min-height: calc(100vh - 7rem);" pb-14>
       <slot />
     </div>
     <ClientOnly>
